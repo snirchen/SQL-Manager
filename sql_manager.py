17d494f3aca27c76b65828ee57c54dd4f853e4e0
Snir Chen <snirchen@gmail.com>
2024-09-04 00:57:47 +0300

    Added execute_sql_query function

--- sql_manager.py
+++ sql_manager.py
@@ -1,7 +1,27 @@
+import sqlite3
 import threading
+from typing import Tuple, Any, List
 
 
 class SqlManager:
     def __init__(self, db):
         self.mutex = threading.Lock()
         self.db = db
+
+    def execute_sql_query(self, query, parameters=None) -> List[Tuple[Any, ...]]:
+        try:
+            self.mutex.acquire()
+            con = sqlite3.connect(self.db)
+            cur = con.cursor()
+            if parameters:
+                cur.execute(query, parameters)
+            else:
+                cur.execute(query)
+            result = cur.fetchall()
+            con.commit()
+            con.close()
+            return result
+        except Exception as e:  # noqa
+            return []
+        finally:
+            self.mutex.release()
