ed22c04e114ba4b7168e6cbcc05dffa2ba0b3f22
Snir Chen <snirchen@gmail.com>
2024-09-04 00:59:06 +0300

    Added create_table_if_not_exists function

--- sql_manager.py
+++ sql_manager.py
@@ -25,3 +25,22 @@ class SqlManager:
             return []
         finally:
             self.mutex.release()
+
+    def create_table_if_not_exists(self, table_name: str, fields: dict = None,
+                                   foreign_keys: dict = None) -> List[Tuple[Any, ...]]:
+        fields_as_str = ''
+        foreign_keys_as_str = ''
+
+        if fields:
+            for field in fields:
+                fields_as_str += f'{field} {fields[field]}, '
+            fields_as_str = fields_as_str[:-2]
+
+        if foreign_keys:
+            for foreign_key in foreign_keys:
+                foreign_keys_as_str += f'FOREIGN KEY({foreign_key}) REFERENCES {foreign_keys[foreign_key]}, '
+            foreign_keys_as_str = foreign_keys_as_str[:-2]
+
+        table_data = fields_as_str if foreign_keys is None else f'{fields_as_str}, {foreign_keys_as_str}'
+
+        return self.execute_sql_query(f'CREATE TABLE IF NOT EXISTS {table_name}({table_data})')
