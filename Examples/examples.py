23ac776c24e92d5e00d2624e4b670c3270e7c514
Snir Chen <snirchen@gmail.com>
2024-09-04 23:26:36 +0300

    Added examples of all the functionalities of sql_manger

--- Examples/examples.py
+++ Examples/examples.py
@@ -1,9 +1,19 @@
+import uuid
 from pathlib import Path
 
 from sql_manager import SqlManager
 
+CLIENT_1_UUID = uuid.uuid4().bytes.hex()
+CLIENT_1_NAME = "Bob"
 
-def add_example_tables(sql_manager: SqlManager) -> None:
+CLIENT_2_UUID = uuid.uuid4().bytes.hex()
+CLIENT_2_NAME = "Alice"
+
+MESSAGE_1_UUID = uuid.uuid4().bytes.hex()
+MESSAGE_2_UUID = uuid.uuid4().bytes.hex()
+
+
+def __add_example_tables(sql_manager: SqlManager) -> None:
     sql_manager.create_table(table_name="clients",
                              fields={
                                  'ID': 'text NOT NULL PRIMARY KEY',
@@ -25,13 +35,35 @@ def add_example_tables(sql_manager: SqlManager) -> None:
                              throw_if_exists=False)
 
 
+def __insert_example_data_to_tables(sql_manager: SqlManager) -> None:
+    sql_manager.insert(table_name="clients", values=[CLIENT_1_UUID, CLIENT_1_NAME])
+    sql_manager.insert(table_name="clients", values=[CLIENT_2_UUID, CLIENT_2_NAME])
+    sql_manager.insert(table_name="messages", values=[MESSAGE_1_UUID, CLIENT_1_UUID, CLIENT_2_UUID, "Hello Alice!"])
+    sql_manager.insert(table_name="messages", values=[MESSAGE_2_UUID, CLIENT_2_UUID, CLIENT_1_UUID, "Hello Bob!"])
+
+
 def main() -> None:
     EXAMPLE_DB_PATH = Path("server.db")
     open(EXAMPLE_DB_PATH, 'a').close()
 
     sql_manager = SqlManager(EXAMPLE_DB_PATH)
 
-    add_example_tables(sql_manager)
+    __add_example_tables(sql_manager)
+    __insert_example_data_to_tables(sql_manager)
+
+    print("Select all clients:")
+    print(f"{sql_manager.select(select='*', from_='clients')}\n")
+
+    print("Select content of each message:")
+    print(sql_manager.select(select='Content', from_="messages", FromClient=CLIENT_1_UUID))
+    print(sql_manager.select(select='Content', from_="messages", FromClient=CLIENT_2_UUID))
+
+    print("\nSelect all messages:")
+    print(f"{sql_manager.select(select='*', from_='messages')}\n")
+
+    sql_manager.delete(from_="messages", ID=MESSAGE_1_UUID)
+    print("Select all messages after deleted MESSAGE_1:")
+    print(sql_manager.select(select='*', from_="messages"))
 
 
 if __name__ == '__main__':
